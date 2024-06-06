41a8c1b68a0967f41db133f20046381970cf0034
coispers <fmosuela12@gmail.com>
2024-06-07 02:07:50 +0800

    i ended at 1:25:55

--- Admin/rooms.php
+++ Admin/rooms.php
@@ -155,7 +155,7 @@ session_regenerate_id(true);
     <!-- Edit Room Modal -->
     <div class="modal fade" id="edit-room" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
-            <form id="add_room_form">
+            <form id="edit_room_form">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title color-pink">Edit Room</h5>
@@ -232,7 +232,7 @@ session_regenerate_id(true);
                             <label class="form-label fw-bold">Description</label>
                             <textarea name="desc" rows="4" class="form-control shadow-none" required></textarea>
                         </div>
-
+                        <input type="hidden" name="room_id">
                     <div class="modal-footer">
                         <button type="button" class="btn btn-outline-dark" data-bs-dismiss="modal">Cancel</button>
                         <button type="submit" class="btn btn-dark text-white shadow-none">Submit</button>
@@ -326,6 +326,120 @@ session_regenerate_id(true);
 
             xhr.send('get_all_rooms');
         }
+        
+        let edit_room_form = document.getElementById('edit_room_form');
+       
+        //check my bracketing here pls im so tired by this point huhuhu pls check this 
+        function edit_details(id)
+        {
+        {
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", "ajax/rooms.php", true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+            xhr.onload = function(){
+                if (this.status >= 200 && this.status < 300) {
+                    let data = JSON.parse(this.responseText);
+                    edit_room_form.elements['name'].value = data.roomdata.name;
+                    edit_room_form.elements['quantity'].value = data.roomdata.quantity;
+                    edit_room_form.elements['adult'].value = data.roomdata.adult;
+                    edit_room_form.elements['children'].value = data.roomdata.children;
+                    edit_room_form.elements['price'].value = data.roomdata.price;
+                    edit_room_form.elements['desc'].value = data.roomdata.desc;
+                    edit_room_form.elements['room_id'].value = data.roomdata.id;
+
+                    edit_room_form.elements['features'].forEach(element => {
+                    if (data.features.includes(Number(element.value))) {
+                        element.checked = true;
+                    }
+                    });
+
+                    edit_room_form.elements['facilities'].forEach(element => {
+                    if (data.facilities.includes(Number(element.value))) {
+                        element.checked = true;
+                    }
+                    });
+                    } else {
+                        //do nothing 
+                    }
+                } else {
+                    console.error("Failed to load data. Status:", this.status);
+                }
+            };
+
+            xhr.onerror = function() {
+                console.error("Network error occurred.");
+            };
+
+            xhr.send('get_room='+id);
+        }
+        
+        edit_room_form.addEventListener('submit', function(e){
+            e.preventDefault();
+            submit_edit_room();
+        });
+
+        function submit_edit_room(){
+            let data = new FormData(add_room_form); // Correctly bind form data
+            data.append('edit_room', '');
+            data.append('room_id', edit_room_form.elements['room_id'].value);
+
+            // Append additional form data
+            data.append('name', edit_room_form.elements['name'].value);
+            data.append('price', edit_room_form.elements['price'].value);
+            data.append('quantity', edit_room_form.elements['quantity'].value);
+            data.append('adult', edit_room_form.elements['adult'].value);
+            data.append('children', edit_room_form.elements['children'].value);
+            data.append('desc', edit_room_form.elements['desc'].value);
+
+            let feature = [];
+            edit_room_form.elements['feature'].forEach(element => {
+                if (element.checked) {
+                    feature.push(element.value);
+                }
+            });
+
+            let facilities = [];
+            edit_room_form.elements['facilities'].forEach(element => {
+                if (element.checked) {
+                    facilities.push(element.value);
+                }
+            });
+
+            data.append('feature', JSON.stringify(feature));
+            data.append('facilities', JSON.stringify(facilities));
+
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", "ajax/rooms.php", true);
+
+            xhr.onload = function(){
+                var myModal = document.getElementById('edit-room');
+                var modal = bootstrap.Modal.getInstance(myModal);
+                modal.hide();
+
+                if (this.status >= 200 && this.status < 300) {
+                    console.log("Response:", this.responseText);
+                    var response = JSON.parse(this.responseText);
+                    if (response == 1) {
+                        alert('success', "Room data edited!");
+                        edit_room_form.reset();
+                        get_all_rooms();
+                    } else {
+                        alert('error', "No Changes Saved!");
+                    }
+                } else {
+                    console.error("Failed to load data. Status:", this.status);
+                }
+            };
+
+            xhr.onerror = function() {
+                console.error("Network error occurred.");
+            };
+
+            xhr.send(data);
+        
+        }
+
         function toggle_status(id, val){
             let xhr = new XMLHttpRequest();
             xhr.open("POST", "ajax/rooms.php", true);
